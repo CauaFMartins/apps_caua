3fe38a43d9653b8447e01f9f425f764f204cb9b0
Cauã Martins <cauafmartins01@gmail.com>
2025-11-11 10:04:18 -0300

    oi

---
+++ react-caua-apps/exercicios/1_b.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function OlaMundo() {
+  return <h1>Olá, Mundo!</h1>;
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <OlaMundo />
+      <h2>❤️</h2>
+    </div>
+  );
+}
